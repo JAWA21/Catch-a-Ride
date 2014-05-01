f0b61bcfefa897f15a0dc720271adb7c1f88c3a6
Jeanna <jawa@fullsail.edu>
2014-05-01 12:34:10 -0400

    pushing updates of login and weather working

--- catch/app/scripts/app.js
+++ catch/app/scripts/app.js
@@ -2,13 +2,38 @@
 
 angular
   .module('catchApp', [
-    'ngRoute'
+    'ngRoute',
+      'firebase'
   ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'views/main.tpl',
         controller: 'MainCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.tpl',
+        controller: 'MyAuthCtrl'
+      })
+      .when('/signup', {
+        templateUrl: 'views/signup.tpl',
+        controller: 'UserCtrl'
+      })
+      .when('/home', {
+        templateUrl: 'views/home.tpl',
+        controller: 'HomeCtrl'
+      })
+      .when('/profile', {
+        templateUrl: 'views/profile.tpl',
+        controller: 'HomeCtrl'
+      })
+      .when('/available', {
+        templateUrl: 'views/availTrans.tpl',
+        controller: 'HomeCtrl'
+      })
+       .when('/rides', {
+        templateUrl: 'views/yourRides.tpl',
+        controller: 'HomeCtrl'
       })
       .otherwise({
         redirectTo: '/'
